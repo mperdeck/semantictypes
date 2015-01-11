680a1f32ba4e3744cf82dbbfd01a2e6ac47ad6dc
mperdeck <mperdeck@gmail.com>
2015-01-11 18:43:24 +1100

    0.9.1

--- SemanticTypes.Examples/SemanticTypeExamples/EmailAddress.cs
+++ SemanticTypes.Examples/SemanticTypeExamples/EmailAddress.cs
@@ -20,6 +20,11 @@ namespace SemanticTypes.SemanticTypeExamples
         // and storage in the Value property.
         public EmailAddress(string emailAddress) : base(IsValid, emailAddress) { }
 
+        // ---------------------------------------
+        // Be careful with implementing casts, and especially implicit casts. They move you away from 
+        // compile time checking to run time checking. Also, going from EmailAddress to string and vice versa
+        // shouldn't be a common thing, so having a cast will not save you a lot of typing.
+
         public static explicit operator string(EmailAddress value)
         {
             return value.Value;
